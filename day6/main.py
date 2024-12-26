869b287391391b0ebd362496497f0ee3ed791171
Michael Mumme <neptica@hotmail.com>
2024-12-26 14:33:11 -0600

    d6pt2 try

--- day6/main.py
+++ day6/main.py
@@ -1,102 +1,101 @@
-import sys
-
-sys.setrecursionlimit(50000)
-
-
 def read_file(filename):
-    results = []
+    results = set()
     guard = (0, 0)
     with open(filename, "r", encoding="utf-8") as file:
-        for i, line in enumerate(file):
-            results.append(list(line.strip()))
-            pos = line.find("^")
-            if pos > -1:
-                guard = (i, pos)
-    return results, guard
+        m = file.readlines()
+        y = len(m)
+        x = len(m[0].strip())
+        for i, line in enumerate(m):
+            for j, c in enumerate(line):
+                if c == "#":
+                    results.add((i, j))
+                elif c == "^":
+                    guard = (i, j)
+
+        m = [list(line.strip()) for line in m]
+        return results, guard, y, x, m
 
 
-def dfs(m, p, d, visited, turns, checking):
-    if (p[0], p[1], d) in turns and len(turns) > 1:
-        return 1
-
-    if not checking:
-        visited.add((p[0], p[1]))
-
-    directions = ["^", ">", "v", "<"]
-    prev = m[p[0]][p[1]]
-    m[p[0]][p[1]] = directions[d]
-    m[p[0]][p[1]] = prev
-
-    match d:
-        case 0:
-            if p[0] > 0:
-                check = 0
-
-                if m[p[0] - 1][p[1]] == "#":
-                    turns.add((p[0], p[1], d))
-                    return dfs(m, (p[0], p[1]), 1, visited, turns, checking)
-
-                if (p[0] - 1, p[1]) not in visited and not checking:
-                    check = dfs(m, (p[0], p[1]), 1, visited, { (p[0], p[1], 0) }, True)
-                    turns = set()
-                return dfs(m, (p[0] - 1, p[1]), d, visited, turns, checking) + check
-
-            return 0
-        case 1:
-            if p[1] + 1 < len(m[0]):
-                check = 0
-
-                if m[p[0]][p[1] + 1] == "#":
-                    turns.add((p[0], p[1], d))
-                    return dfs(m, (p[0], p[1]), 2, visited, turns, checking)
-
-                if (p[0], p[1] + 1) not in visited and not checking:
-                    check = dfs(m, (p[0], p[1]), 2, visited, { (p[0], p[1], 1) }, True)
-                    turns = set()
-
-                return dfs(m, (p[0], p[1] + 1), d, visited, turns, checking) + check
-            return 0
-        case 2:
-            if p[0] + 1 < len(m):
-                check = 0
-
-                if m[p[0] + 1][p[1]] == "#":
-                    turns.add((p[0], p[1], d))
-                    return dfs(m, (p[0], p[1]), 3, visited, turns, checking)
-
-                if (p[0] + 1, p[1]) not in visited and not checking:
-                    check = dfs(m, (p[0], p[1]), 3, visited, { (p[0], p[1], 2) }, True)
-                    turns = set()
-
-                return dfs(m, (p[0] + 1, p[1]), d, visited, turns, checking) + check
-            return 0
-
-        case 3:
-            if p[1] > 0:
-                check = 0
-
-                if m[p[0]][p[1] - 1] == "#":
-                    turns.add((p[0], p[1], d))
-                    return dfs(m, (p[0], p[1]), 0, visited, turns, checking)
-
-                if (p[0], p[1] - 1) not in visited and not checking:
-                    check = dfs(m, (p[0], p[1]), 0, visited, { (p[0], p[1], 3) }, True)
-                    turns = set()
-
-                return dfs(m, (p[0], p[1] - 1), d, visited, turns, checking) + check
-            return 0
-    return 0
+blocks, gloc, h, w, m = read_file("./input.txt")
 
 
-if __name__ == "__main__":
-    data = read_file("./input.txt")
+def check(blocks, pos, d):
+    fpos = pos + d
+    if (int(fpos.imag), int(fpos.real)) in blocks:
+        fpos -= d
+        d *= 1j
+        fpos += d
+    fd = d
+    while -1 < int(fpos.imag) < h and -1 < int(fpos.real) < w:
+        # ensure slow moves
+        m[int(pos.imag)][int(pos.real)] = "S"
+        m[int(fpos.imag)][int(fpos.real)] = "F"
+        m[int(pos.imag)][int(pos.real)] = "."
+        m[int(fpos.imag)][int(fpos.real)] = "."
+        while True:
+            pos += d
+            if (int(pos.imag), int(pos.real)) in blocks:
+                pos -= d
+                d *= 1j
+            else:
+                break
 
-    for l in data[0]:
-        for c in l:
-            print(c, end="")
-        print()
-    print(data[1])
+        # fast moves twice
+        n = 0
+        while n < 2:
+            fpos += fd
+            if (int(fpos.imag), int(fpos.real)) in blocks:
+                fpos -= fd
+                fd *= 1j
+            else:
+                n += 1
 
-    visited = { (y, data[1][1]) for y in range(data[1][0] + 1) }
-    ans = dfs(data[0], data[1], 0, set(), set(), False)
+        if pos == fpos and fd == d:
+            return True
+
+    return False
+
+
+def part_one():
+    pos = gloc[1] + gloc[0] * 1j
+    d = -1j
+    ans = 0
+    been = set()
+    for y in range(int(pos.imag), -1, -1):
+        if m[y][gloc[1]] == "#":
+            break
+        been |= {gloc[1] + y * 1j}
+
+    while -1 < int(pos.imag + d.imag) < h and -1 < int(pos.real + d.real) < w:
+        while (
+            (int(pos.imag), int(pos.real)) not in blocks
+            and -1 < int(pos.imag + d.imag) < h
+            and -1 < int(pos.real + d.real) < w
+        ):
+            m[int(pos.imag)][int(pos.real)] = "C"
+            m[int(pos.imag)][int(pos.real)] = "."
+            if (
+                int(pos.imag + d.imag),
+                int(pos.real + d.real),
+            ) not in blocks and pos + d not in been:
+                been.add(pos)
+                m[int(pos.imag + d.imag)][int(pos.real + d.real)] = "#"
+                curr = (int(pos.imag + d.imag), int(pos.real + d.real))
+                ans += check(
+                    blocks | {curr},
+                    pos,
+                    d * 1j,
+                )
+                m[int(pos.imag + d.imag)][int(pos.real + d.real)] = "."
+            elif (
+                int(pos.imag + d.imag),
+                int(pos.real + d.real),
+            ) in blocks:
+                d *= 1j
+
+            pos += d
+
     print(ans)
+
+
+part_one()
