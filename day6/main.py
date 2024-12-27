22149733bb7ebbdc889139e80039976ca4b6a31a
Michael Mumme <neptica@hotmail.com>
2024-12-27 14:56:18 -0600

    day6 pt2 solution

--- day6/main.py
+++ day6/main.py
@@ -10,7 +10,7 @@ def read_file(filename):
                 if c == "#":
                     results.add((i, j))
                 elif c == "^":
-                    guard = (i, j)
+                    guard = j + i * 1j
 
         m = [list(line.strip()) for line in m]
         return results, guard, y, x, m
@@ -19,83 +19,19 @@ def read_file(filename):
 blocks, gloc, h, w, m = read_file("./input.txt")
 
 
-def check(blocks, pos, d):
-    fpos = pos + d
-    if (int(fpos.imag), int(fpos.real)) in blocks:
-        fpos -= d
-        d *= 1j
-        fpos += d
-    fd = d
-    while -1 < int(fpos.imag) < h and -1 < int(fpos.real) < w:
-        # ensure slow moves
-        m[int(pos.imag)][int(pos.real)] = "S"
-        m[int(fpos.imag)][int(fpos.real)] = "F"
-        m[int(pos.imag)][int(pos.real)] = "."
-        m[int(fpos.imag)][int(fpos.real)] = "."
-        while True:
-            pos += d
-            if (int(pos.imag), int(pos.real)) in blocks:
-                pos -= d
-                d *= 1j
-            else:
-                break
-
-        # fast moves twice
-        n = 0
-        while n < 2:
-            fpos += fd
-            if (int(fpos.imag), int(fpos.real)) in blocks:
-                fpos -= fd
-                fd *= 1j
-            else:
-                n += 1
-
-        if pos == fpos and fd == d:
-            return True
-
-    return False
+def walk(blocks, pos, d):
+    ans = set()
+    while -1 < int(pos.imag) < h and -1 < int(pos.real) < w and (pos, d) not in ans:
+        ans.add((pos, d))
+        pos += d
+        if (int(pos.imag), int(pos.real)) in blocks:
+            pos -= d
+            d *= 1j
+    return {p for p, _ in ans}, (pos, d) in ans
 
 
-def part_one():
-    pos = gloc[1] + gloc[0] * 1j
-    d = -1j
-    ans = 0
-    been = set()
-    for y in range(int(pos.imag), -1, -1):
-        if m[y][gloc[1]] == "#":
-            break
-        been |= {gloc[1] + y * 1j}
-
-    while -1 < int(pos.imag + d.imag) < h and -1 < int(pos.real + d.real) < w:
-        while (
-            (int(pos.imag), int(pos.real)) not in blocks
-            and -1 < int(pos.imag + d.imag) < h
-            and -1 < int(pos.real + d.real) < w
-        ):
-            m[int(pos.imag)][int(pos.real)] = "C"
-            m[int(pos.imag)][int(pos.real)] = "."
-            if (
-                int(pos.imag + d.imag),
-                int(pos.real + d.real),
-            ) not in blocks and pos + d not in been:
-                been.add(pos)
-                m[int(pos.imag + d.imag)][int(pos.real + d.real)] = "#"
-                curr = (int(pos.imag + d.imag), int(pos.real + d.real))
-                ans += check(
-                    blocks | {curr},
-                    pos,
-                    d * 1j,
-                )
-                m[int(pos.imag + d.imag)][int(pos.real + d.real)] = "."
-            elif (
-                int(pos.imag + d.imag),
-                int(pos.real + d.real),
-            ) in blocks:
-                d *= 1j
-
-            pos += d
-
-    print(ans)
-
-
-part_one()
+walked = walk(blocks, gloc, -1j)[0]
+print(
+    len(walked),
+    sum(walk(blocks | {(int(p.imag), int(p.real))}, gloc, -1j)[1] for p in walked),
+)
